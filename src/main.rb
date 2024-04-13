b307bc9faccab4856fccab5f261e6b646f60990b
Brandon Scott <contact@brandonscott.me>
2024-04-13 11:55:25 -0400

    refactor: run workflow every hour and split artists.json file into even chunks (#2)
    * refactor: run workflow every hour and split artists.json file into even chunks
    
    * fix: use more than one api key depending on hour
    
    * Latest data: Sat Apr 13 15:52:36 UTC 2024
    
    * fix: hour is already zero-based, so no need to subtract one
    
    ---------
    
    Co-authored-by: Github Actions <actions@users.noreply.github.com>

--- src/main.rb
+++ src/main.rb
@@ -1,23 +1,33 @@
 require 'bundler/setup'
 require 'json'
 require 'rspotify'
-
-
+require 'date'
 
 def main()
-    RSpotify.authenticate(ENV["CLIENT_ID"], ENV["CLIENT_SECRET"])
+    clientIds = ENV["CLIENT_IDS"].split(',')
+    clientSecrets = ENV["CLIENT_SECRETS"].split(',')
+
+    hour = Integer(DateTime.now.strftime('%H'))
+
+    secretIndex = hour % 8
+    clientId = clientIds[secretIndex]
+    clientSecret = clientSecrets[secretIndex]
+
+    RSpotify.authenticate(clientId, clientSecret)
 
     artist_ids_file = File.read('input/artists.json')
     artist_ids = JSON.parse(artist_ids_file)
+    chunk_size = (artist_ids.length / 24).floor
+    chunk = artist_ids.each_slice(chunk_size).to_a()[hour]
 
     new_artist_ids = []
 
-    artist_ids.each_slice(50) do |artist_ids_chunk|
+    chunk.each_slice(50) do |artist_ids_chunk|
         artists = RSpotify::Artist.find(artist_ids_chunk)
         artists.each_with_index do |artist, index|
             # Manually slow down the loop every 5th iteration since we're making an additional API call per artist we track
             if index % 5 === 0
-                sleep(1)
+                sleep(0.5)
             end
 
             save_artist_json_file(artist)
