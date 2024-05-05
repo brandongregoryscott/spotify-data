a6ba0af71d57f97eefd98978f0d173a1ebcaf574
Brandon Scott <contact@brandonscott.me>
2024-05-04 23:07:32 -0400

    fix: rescue generic ExceptionWithResponse class instead of specific instances that might not exist in related artists script

--- scripts/sample_related_artists.rb
+++ scripts/sample_related_artists.rb
@@ -23,7 +23,7 @@ end
 def find_related_artists(artist, attempt = 1)
   related_artists = artist.related_artists
   related_artists.map(&:id)
-rescue RestClient::TooManyRequests, RestClient::ServiceUnavailable
+rescue RestClient::ExceptionWithResponse
   max_sleep_seconds = Float(2**attempt)
   sleep rand(0..max_sleep_seconds)
   authenticate
