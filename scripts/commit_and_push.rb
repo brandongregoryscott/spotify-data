4f4cc7bf6f1175d7bfad328d00e385926152b2e7
Brandon Scott <contact@brandonscott.me>
2024-06-09 12:46:46 -0400

    feat: add script for committing & pushing to branch for 1 commit per day (#9)

---
+++ scripts/commit_and_push.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'git'
+require 'json'
+require 'date'
+require 'pathname'
+
+def main
+  git = Git.open(Pathname('.'))
+
+  configure_git_user(git)
+  branch_name = checkout_branch(git)
+
+  git.add('output')
+  git.commit(DateTime.now.iso8601)
+
+  if current_hour == 23
+    merge_and_delete_daily_branch(git, branch_name)
+    return
+  end
+
+  git.push('origin', branch_name)
+end
+
+def configure_git_user(git)
+  git.config('user.name', 'Github Actions')
+  git.config('user.email', 'actions@users.noreply.github.com')
+end
+
+def checkout_branch(git)
+  branch_name = current_date
+  is_new_branch = !git.is_branch?(branch_name)
+
+  git.checkout(branch_name, new_branch: is_new_branch)
+
+  branch_name
+end
+
+def merge_and_delete_daily_branch(git, branch_name)
+  git.checkout('main')
+  system("git merge --squash  #{branch_name} -m #{branch_name}")
+  git.push('origin', branch_name, delete: true)
+  git.push('origin', 'main')
+end
+
+def current_date
+  DateTime.now.strftime('%Y-%m-%d')
+end
+
+def current_hour
+  # - flag drops then padding
+  hour = DateTime.now.strftime('%-H')
+  Integer(hour)
+end
+
+main
