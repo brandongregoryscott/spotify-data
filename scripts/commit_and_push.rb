2342da7f54933dc5e6a8906cca007a472e2a3b12
Brandon Scott <contact@brandonscott.me>
2024-06-10 19:18:57 -0400

    fix: separate checkout and commit/push steps to prevent merge conflict issues

--- scripts/commit_and_push.rb
+++ scripts/commit_and_push.rb
@@ -2,18 +2,16 @@
 
 require 'bundler/setup'
 require 'git'
-require 'json'
 require 'date'
 require 'pathname'
 require 'logger'
 
 
 def main
-  logger = Logger.new(STDOUT)
+  logger = Logger.new($stdout)
   git = Git.open(Pathname('.'), log: logger)
 
-  configure_git_user(git)
-  branch_name = checkout_branch(git)
+  branch_name = git.current_branch
 
   git.add('output')
   git.commit(DateTime.now.iso8601)
@@ -26,36 +24,12 @@ def main
   git.push('origin', branch_name)
 end
 
-def configure_git_user(git)
-  git.config('user.name', 'Github Actions')
-  git.config('user.email', 'actions@users.noreply.github.com')
-end
-
-def checkout_branch(git)
-  git.fetch('origin')
-
-  branch_name = current_date
-  is_new_branch = !git.is_branch?(branch_name)
-
-  git.checkout(branch_name, new_branch: is_new_branch)
-
-  git.pull('origin', branch_name)
-  git.pull('origin', 'main')
-  branch_name
-rescue Git::FailedError
-  branch_name
-end
-
 def merge_and_delete_daily_branch(git, branch_name)
   git.checkout('main')
   system("git merge --squash  #{branch_name}")
   git.commit(branch_name)
   git.push('origin', branch_name, delete: true)
   git.push('origin', 'main')
-end
-
-def current_date
-  DateTime.now.strftime('%Y-%m-%d')
 end
 
 def current_hour
