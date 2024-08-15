2a0307c02e9562da2548d35f59edbce5542fe215
Brandon Scott <contact@brandonscott.me>
2024-08-14 21:27:45 -0400

    fix: handle non-200 status code returns from curl request

--- scripts/start_workflow.sh
+++ scripts/start_workflow.sh
@@ -1,7 +1,14 @@
-curl -L \
+STATUS_CODE=$(curl -L \
+  -o /dev/null -s -w "%{http_code}\n" \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_ACCESS_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/brandongregoryscott/spotify-data/actions/workflows/92677804/dispatches \
-  -d '{"ref":"main"}'
+  -d '{"ref":"main"}')
+
+
+if [[ $STATUS_CODE -ne '200' ]]
+then
+  exit 1;
+fi;
