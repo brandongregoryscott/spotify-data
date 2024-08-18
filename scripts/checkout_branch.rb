a26965c9dacd013457b34701039eeff3adfd29e6
Brandon Scott <contact@brandonscott.me>
2024-08-17 23:48:38 -0400

    Revert "fix: skip pull if cache hit" (#21)
    * Revert "fix: skip pull if cache hit (#20)"
    
    This reverts commit 6c2685fde58c3e131f8843e24741ca2bc09f38fb.
    
    * fix: skip clean in checkout action
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- scripts/checkout_branch.rb
+++ scripts/checkout_branch.rb
@@ -5,16 +5,13 @@ require 'git'
 require 'date'
 require 'pathname'
 require 'logger'
-require 'optparse'
 
 def main
   logger = Logger.new($stdout)
   git = Git.open(Pathname('.'), log: logger)
 
-  options = parse_options
-
   configure_git_user(git)
-  checkout_branch(git, options)
+  checkout_branch(git)
 end
 
 def configure_git_user(git)
@@ -23,33 +20,16 @@ def configure_git_user(git)
   git.config('pull.rebase', 'false')
 end
 
-def checkout_branch(git, options)
-  git.fetch('origin', unshallow: true) unless options[:skip_pull]
+def checkout_branch(git)
+  git.fetch('origin', unshallow: true)
 
   branch_name = current_date
   is_new_branch = !git.is_branch?(branch_name)
 
   git.checkout(branch_name, new_branch: is_new_branch)
 
-  return if options[:skip_pull]
-
   git.pull('origin', branch_name) unless is_new_branch
   git.pull('origin', 'main')
-end
-
-def parse_options
-  options = {}
-
-  OptionParser.new do |opts|
-    opts.banner = 'Usage: ruby scripts/checkout_branch.rb [options]'
-
-    opts.on('--skip-pull [cache-matched-key]',
-            'Whether to skip fetching/pulling from the remote branch in the event of a cache hit') do |skip_pull|
-      options[:skip_pull] = !skip_pull.nil? && !skip_pull.empty?
-    end
-  end.parse!
-
-  options
 end
 
 def current_date
